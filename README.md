cf8c2385f873620c1a5413dfa9ce1c325f69dcc3
Maksym Korchak <Mkorchak1992@gmail.com>
2022-10-09 21:29:01 +0300

    Add hacker-news server part

---
+++ README.md
@@ -0,0 +1,3 @@
+# hackernews-graphql-js
+
+This repository contains the final project for the [**GraphQL.js tutorial**](https://www.howtographql.com/graphql-js/0-introduction/) on [How to GraphQL](https://www.howtographql.com/).
---
+++ src/index.js
@@ -0,0 +1,50 @@
+const { ApolloServer, PubSub } = require('apollo-server');
+const fs = require('fs');
+const path = require('path');
+const { PrismaClient } = require('@prisma/client')
+const Query = require('./resolvers/Query');
+const Mutation = require('./resolvers/Mutation');
+const User = require('./resolvers/User');
+const Link = require('./resolvers/Link');
+const Subscription = require('./resolvers/Subscription');
+const Vote = require('./resolvers/Vote');
+const { getUserId } = require('./utils');
+
+const prisma = new PrismaClient();
+const pubsub = new PubSub();
+
+const resolvers = {
+  Query,
+  Mutation,
+  Subscription,
+  User,
+  Link,
+  Vote,
+}
+
+const typeDefs = fs.readFileSync(
+  path.join(__dirname, 'schema.graphql'),
+  'utf-8'
+)
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ( {req} ) => {
+    return {
+      ...req,
+      prisma,
+      pubsub,
+      userId: 
+        req && req.headers.authorization
+          ? getUserId(req)
+          : null
+    }
+  }
+})
+
+server
+  .listen()
+  .then(({ url }) =>
+    console.log(`Server is running on ${url}`)
+  );
